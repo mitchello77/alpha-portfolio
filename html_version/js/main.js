5447ea7083b5f4684a9a29478ff2fc0909e6f6fc
Mitchell Williams <mwm1503@gmail.com>
2016-10-27 22:00:38 +1000

    custom animate.css build

--- html_version/js/main.js
+++ html_version/js/main.js
@@ -1,13 +1,18 @@
+// Varibles
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 $(function() {
+  // Varibles after DOM construction
+  var objIntro = $('#intro');
+  // -------------------
   console.log("JQuery Version: " + $.fn.jquery );
-  $(window).scroll(function () {
-    if ($(window).scrollTop() >= 70) {
-      $('header').removeClass('fixed');
-    }
-    if ($(window).scrollTop() < 69) {
-      $('header').addClass('fixed');
-    }
-  });
+  if (objIntro !=== undefined) {
+    $(window).scroll(function () {
+      if (objIntro.hasClass('hidden') == false) {
+        hideIntro(objIntro);
+      }
+    });
+  }
+
   //scroll Reveal
   window.sr = ScrollReveal();
   sr.reveal('#mini-skills .col',{origin: 'left', delay: '250', distance: '40px'});
@@ -22,4 +27,23 @@ $(function() {
     dotsEach: true,
     loop: true
   });
+});
+
+hideIntro = function(obj) {
+  window.scrollTo(0,0);
+  // animate it away
+  obj.animateCss('bounceOutUp');
+    obj.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+      window.scrollTo(0,0);
+      obj.addClass('hidden');
+    });
+}
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
 });
