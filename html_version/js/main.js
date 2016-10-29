3499f0bf21810199f43dca54d1ed51a476a51c7e
Mitchell Williams <mwm1503@gmail.com>
2016-10-30 02:35:47 +1000

    update intro

--- html_version/js/main.js
+++ html_version/js/main.js
@@ -1,25 +1,44 @@
 // Varibles
 var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+var intropresent = false;
+var theater = theaterJS({"autoplay": false, "minSpeed": 60, "maxSpeed": 450});
+
+$(window).on('load', function() { //insync with preloader finish
+  if (intropresent == true) {
+      theater.play();
+      $('.twentytwenty-handle').mousedown( function() {
+        $('.prompt').fadeOut();
+      });
+  };
+});
+
 $(function() {
   // Varibles after DOM construction
   var objIntro = $('#intro');
   // -------------------
   console.log("JQuery Version: " + $.fn.jquery );
-  if (objIntro !=== undefined) {
+  if (objIntro !== undefined) {
+    intropresent = true;
     $(window).scroll(function () {
       if (objIntro.hasClass('hidden') == false) {
         hideIntro(objIntro);
       }
     });
-  }
+  };
 
   //scroll Reveal
   window.sr = ScrollReveal();
   sr.reveal('#mini-skills .col',{origin: 'left', delay: '250', distance: '40px'});
 
   $('#before-after').imagesLoaded( function() {
-    $('#before-after').twentytwenty();
+    $('#before-after').twentytwenty({
+      default_offset_pct: 0.30
+    });
   });
+
+  //theater
+  theater.addActor('typer').addScene('typer:...',3000).addScene('typer:Coder.',2000).addScene('typer:Designer.',2000).addScene('typer:Technology Lover.',2000).addScene('typer:Nerd?',500).addScene(theater.replay);
+
 
   $("#featured-projects .owl-carousel").owlCarousel({
     margin: 20,
@@ -32,6 +51,8 @@ $(function() {
 hideIntro = function(obj) {
   window.scrollTo(0,0);
   // animate it away
+  theater.stop();
+  theater = null;
   obj.animateCss('bounceOutUp');
     obj.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
       window.scrollTo(0,0);
