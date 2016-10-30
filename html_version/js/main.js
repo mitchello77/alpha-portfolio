8383dea11fd42ad4c433cebd8b7c55d5172bb53a
Mitchell Williams <mwm1503@gmail.com>
2016-10-30 15:02:48 +1000

    add contact form

--- html_version/js/main.js
+++ html_version/js/main.js
@@ -19,6 +19,8 @@ $(function() {
   console.log("JQuery Version: " + $.fn.jquery );
   if (objIntro !== undefined) {
     intropresent = true;
+    // FitText
+    $('#intro h1').fitText(1.22);
     $(window).scroll(function () {
       if (objIntro.hasClass('hidden') == false) {
         hideIntro(objIntro);
@@ -37,8 +39,7 @@ $(function() {
   });
 
   //theater
-  theater.addActor('typer').addScene('typer:...',3000).addScene('typer:Coder.',2000).addScene('typer:Designer.',2000).addScene('typer:Technology Lover.',2000).addScene('typer:Nerd?',500).addScene(theater.replay);
-
+  theater.addActor('typer').addScene('typer:...',3000).addScene('typer:Coder.',2000).addScene('typer:Designer.',2000).addScene('typer:Tech Lover.',2000).addScene('typer:Nerd?',500).addScene(theater.replay);
 
   $("#featured-projects .owl-carousel").owlCarousel({
     margin: 20,
@@ -46,7 +47,8 @@ $(function() {
     dotsEach: true,
     loop: true
   });
-});
+
+}); // END Window Ready
 
 hideIntro = function(obj) {
   window.scrollTo(0,0);
@@ -68,3 +70,47 @@ $.fn.extend({
         });
     }
 });
+
+/*global jQuery */
+/*!
+* FitText.js 1.2
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
