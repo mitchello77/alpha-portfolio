ffcca098eb35a49f08f54185c9f387fdbc93d39c
Mitchell Williams <mwm1503@gmail.com>
2016-10-31 14:46:32 +1000

    add underscore.me WP theme

---
+++ mitchell-alphaportfolio/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Alpha-Portfolio
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mitchell-alphaportfolio' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mitchell-alphaportfolio' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mitchell-alphaportfolio' ), 'mitchell-alphaportfolio', '<a href="http://mitchell-williams.com.au" rel="designer">Mitchell Williams</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
